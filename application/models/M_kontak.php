6ef67803cd72037c0b545c8c84088b07f8b72e0e
topyk27 <taufikdwiwahyuputra@gmail.com>
2022-11-22 20:48:12 +0800

    Update M_kontak.php
    unduh csv kontak berdasarkan tanggal itu saja

--- application/models/M_kontak.php
+++ application/models/M_kontak.php
@@ -116,7 +116,7 @@ class M_kontak extends CI_Model
 		$arr_kontak = [];
 		$arr_header = ['Name','Given Name','Additional Name','Family Name','Yomi Name','Given Name Yomi','Additional Name Yomi','Family Name Yomi','Name Prefix','Name Suffix','Initials','Nickname','Short Name','Maiden Name','Birthday','Gender','Location','Billing Information','Directory Server','Mileage','Occupation','Hobby','Sensitivity','Priority','Subject','Notes','Language','Photo','Group Membership','Phone 1 - Type','Phone 1 - Value','Organization 1 - Type','Organization 1 - Name','Organization 1 - Yomi Name','Organization 1 - Title','Organization 1 - Department','Organization 1 - Symbol','Organization 1 - Location','Organization 1 - Job Description'];
 		array_push($arr_kontak,$arr_header);		
-		$query = $this->db->query("SELECT b.telepon, b.nama, c.nomor_perkara FROM $this->database.perkara_pihak1 a JOIN $this->database.pihak b ON a.pihak_id=b.id JOIN $this->database.perkara c WHERE c.tanggal_pendaftaran >= '$this->mulai_kontak' AND c.perkara_id=a.perkara_id AND (b.telepon IS NOT NULL AND b.telepon <> '' AND b.telepon <> '-')");		
+		$query = $this->db->query("SELECT b.telepon, b.nama, c.nomor_perkara FROM $this->database.perkara_pihak1 a JOIN $this->database.pihak b ON a.pihak_id=b.id JOIN $this->database.perkara c WHERE c.tanggal_pendaftaran = '$this->mulai_kontak' AND c.perkara_id=a.perkara_id AND (b.telepon IS NOT NULL AND b.telepon <> '' AND b.telepon <> '-')");		
 		if($query->num_rows() > 0)
 		{
 			foreach($query->result() as $row)
@@ -126,7 +126,7 @@ class M_kontak extends CI_Model
 				$arr_kontak[] = array($nama,$nama,'','','','','','','','','','','','','','','','','','','','','','','','','','','','Mobile',$telp,'','','','','','','');
 			}			
 		}		
-		$query1 = $this->db->query("SELECT b.telepon, b.nama, c.nomor_perkara FROM $this->database.perkara_pihak2 a JOIN $this->database.pihak b ON a.pihak_id=b.id JOIN $this->database.perkara c WHERE c.tanggal_pendaftaran >= '$this->mulai_kontak' AND c.perkara_id=a.perkara_id AND (b.telepon IS NOT NULL AND b.telepon <> '' AND b.telepon <> '-')");
+		$query1 = $this->db->query("SELECT b.telepon, b.nama, c.nomor_perkara FROM $this->database.perkara_pihak2 a JOIN $this->database.pihak b ON a.pihak_id=b.id JOIN $this->database.perkara c WHERE c.tanggal_pendaftaran = '$this->mulai_kontak' AND c.perkara_id=a.perkara_id AND (b.telepon IS NOT NULL AND b.telepon <> '' AND b.telepon <> '-')");
 		if($query1->num_rows() > 0)
 		{
 			foreach($query1->result() as $row)
@@ -136,7 +136,7 @@ class M_kontak extends CI_Model
 				$arr_kontak[] = array($nama,$nama,'','','','','','','','','','','','','','','','','','','','','','','','','','','','Mobile',$telp,'','','','','','','');
 			}
 		}
-		$query2 = $this->db->query("SELECT DISTINCT b.telepon, b.nama, c.nomor_perkara FROM $this->database.perkara_pengacara a JOIN $this->database.pihak b ON a.pengacara_id=b.id JOIN $this->database.perkara c WHERE c.tanggal_pendaftaran >= '$this->mulai_kontak' AND c.perkara_id=a.perkara_id AND (b.telepon IS NOT NULL AND b.telepon <> '' AND b.telepon <> '-')");
+		$query2 = $this->db->query("SELECT DISTINCT b.telepon, b.nama, c.nomor_perkara FROM $this->database.perkara_pengacara a JOIN $this->database.pihak b ON a.pengacara_id=b.id JOIN $this->database.perkara c WHERE c.tanggal_pendaftaran = '$this->mulai_kontak' AND c.perkara_id=a.perkara_id AND (b.telepon IS NOT NULL AND b.telepon <> '' AND b.telepon <> '-')");		
 		if($query2->num_rows() > 0)
 		{
 			foreach($query2->result() as $row)
